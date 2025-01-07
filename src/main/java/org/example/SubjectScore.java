6d0978b309712da2c15dd8d2f041ff2832550096
Minsol Kim <yhe071624@gmail.com>
2025-01-07 15:40:40 +0900

    second

---
+++ src/main/java/org/example/SubjectScore.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class SubjectScore {
+
+    String subject;
+    int score;
+
+
+}
---
+++ src/main/java/org/example/SubjectTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class SubjectTest {
+
+    public static void main(String[] args) {
+
+        //변수 앞에는 타입
+
+        SubjectScore s1 = new SubjectScore();
+        s1.subject = "국어";
+        s1.score=30;
+
+        SubjectScore s2 = new SubjectScore();
+        s2.subject = "영어";
+        s2.score=40;
+
+        SubjectScore s3 = new SubjectScore();
+        s3.subject = "수학";
+        s3.score=20;
+
+
+    }
+}
